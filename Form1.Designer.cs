4730e017f12246d4aa53e5ae61b064a9e0a60360
samil korkmaz <samil.korkmaz@gmail.com>
2019-11-13 16:31:17 +0200

    Added files

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TemplateMatching
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbInfo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 561);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Match template";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbInfo
+            // 
+            this.lbInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbInfo.Location = new System.Drawing.Point(12, 618);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(308, 23);
+            this.lbInfo.TabIndex = 3;
+            this.lbInfo.Text = "maxValues[0]=...";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 530);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(228, 14);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(37, 32);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 590);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "C:/temp/flappy/ScreenCapture/bin/Debug/test49.bmp";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 650);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lbInfo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Template Matcher";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbInfo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿/* 
+ * Find template images in source image. Uses EmguCV (OpenCV .Net wrapper) 4.1.1, 64bit dlls.
+ * Şamil Korkmaz, November 2019
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using TemplateMatching.Properties;
+
+namespace TemplateMatching
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var birdTemplateFileNames = new List<string>
+                {
+                    "bird_up1",
+                    "bird_up2",
+                    "bird_level",
+                    "bird_down1",
+                    "bird_down2",
+                    "bird_down3",
+                    "bird_down4"
+                };
+                var resourceManager = Resources.ResourceManager;
+                var pipeGapTemplate = new Image<Bgr, byte>((Bitmap)resourceManager.GetObject("pipeGap"));
+                const int nbOfSourceFiles = 500;
+                for (var iSource = 0; iSource < nbOfSourceFiles; iSource++)
+                {
+                    var sourceFileName = "C:/temp/flappy/ScreenCapture/bin/Debug/test" + iSource + ".bmp";
+                    textBox1.Text = sourceFileName;
+                    textBox1.Refresh();
+                    var source = new Image<Bgr, byte>(sourceFileName);
+
+                    var pipeGapTask = Task.Factory.StartNew(() => GetMatch(source, pipeGapTemplate));
+                    var pipeGapMatchData = pipeGapTask.Result;
+
+                    var taskArray = new Task<MatchData>[birdTemplateFileNames.Count];
+                    for (var i = 0; i < birdTemplateFileNames.Count; i++)
+                    {
+                        var birdTemplate = new Image<Bgr, byte>((Bitmap) resourceManager.GetObject(birdTemplateFileNames[i]));
+                        taskArray[i] = Task.Factory.StartNew(() => GetMatch(source, birdTemplate));
+                    }
+                    var maxBirdMatchQuality = 0.0;
+                    var matchBirdRect = new Rectangle(new Point(0, 0), new Size(0, 0));
+                    for (var i = 0; i < birdTemplateFileNames.Count; i++)
+                    {
+                        var matchBirdData = taskArray[i].Result;
+                        if (matchBirdData.Quality < maxBirdMatchQuality) continue;
+                        //Console.WriteLine("Task " + i + " maxVal = " + matchData.Quality);
+                        maxBirdMatchQuality = matchBirdData.Quality;
+                        matchBirdRect = new Rectangle(matchBirdData.RectLocation, matchBirdData.Template.Size);
+                        pictureBox2.Image = matchBirdData.Template.ToBitmap();
+
+                    }
+                    lbInfo.Text = "maxPipeGap = " + pipeGapMatchData.Quality.ToString("F1") + ", maxBird=" + maxBirdMatchQuality.ToString("F1") + 
+                        ", birdCX = " + (matchBirdRect.X + matchBirdRect.Width / 2).ToString("F1") + ", birdCY = " + ((matchBirdRect.Y + matchBirdRect.Height / 2) - 14).ToString("F1");
+                    lbInfo.Refresh();
+                    var imageToShow = source.Copy();
+                    imageToShow.Draw(matchBirdRect, new Bgr(Color.Red), 3);
+                    if (pipeGapMatchData.Quality > 0.5)
+                    {
+                        var pipeGapRect = new Rectangle(pipeGapMatchData.RectLocation, pipeGapMatchData.Template.Size);
+                        imageToShow.Draw(pipeGapRect, new Bgr(Color.Red), 3);
+                    }
+                    pictureBox1.Image = imageToShow.ToBitmap();
+                    pictureBox1.Refresh();
+                    pictureBox2.Refresh();
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show(this, ex.Message);
+            }
+        }
+
+        private class MatchData
+        {
+            public Image<Bgr, byte> Template { get; }
+            public Point RectLocation { get; }
+            public double Quality { get; }
+            public MatchData(Point rectLocation, double quality, Image<Bgr, byte> template)
+            {
+                RectLocation = rectLocation;
+                Quality = quality;
+                Template = template;
+            }
+        }
+
+        private MatchData GetMatch(Image<Bgr, byte> source, Image<Bgr, byte> template)
+        {
+            using (var result = source.MatchTemplate(template, Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed))
+            {
+                double[] currentMinValues, currentMaxValues;
+                Point[] currentMinLocations, currentMaxLocations;
+                result.MinMax(out currentMinValues, out currentMaxValues, out currentMinLocations, out currentMaxLocations);
+                return new MatchData(currentMaxLocations[0], currentMaxValues[0], template);
+            }
+        }
+    }
+}
+
